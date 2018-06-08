a96a524f65ed46e5892225f0f3bff74a85d373fe
Виктор Дорошенко <viktor@Viktors-MacBook-Air.local>
2018-06-08 15:05:03 +0300

    testing

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
 var config = require('./config.json');
 console.log(valueOf(config.port));
 console.log(config.appName);
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
